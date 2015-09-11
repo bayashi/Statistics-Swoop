fb70bf9cc145a24d2c0026764274cb8f86e40e1d
bayashi <bayashi@cpan.org>
2015-09-11 20:51:08 +0900

    switch benchmark module Benchmark to Benchmarks

--- demos/benchmark.pl
+++ demos/benchmark.pl
@@ -1,36 +1,34 @@
 use strict;
 use warnings;
-use Statistics::Swoop;
-use Statistics::Lite qw//;
+use Benchmarks sub {
+    use Statistics::Swoop;
+    use Statistics::Lite qw//;
 
-use Benchmark qw/timethese cmpthese/;
+    my @LIST = (1..10000);
 
-my @LIST = (1..10000);
+    my $swoop = sub {
+        my $ss = Statistics::Swoop->new(\@LIST);
+        return(
+            $ss->sum,
+            $ss->max,
+            $ss->min,
+            $ss->range,
+            $ss->avg,
+        );
+    };
 
-my $swoop = sub {
-    my $ss = Statistics::Swoop->new(\@LIST);
-    return(
-        $ss->sum,
-        $ss->max,
-        $ss->min,
-        $ss->range,
-        $ss->avg,
-    );
+    my $lite = sub {
+        return(
+            Statistics::Lite::sum(@LIST),
+            Statistics::Lite::max(@LIST),
+            Statistics::Lite::min(@LIST),
+            Statistics::Lite::range(@LIST),
+            Statistics::Lite::mean(@LIST),
+        );
+    };
+
+    return {
+        'Swoop' => $swoop,
+        'Lite'  => $lite,
+    };
 };
-
-my $lite = sub {
-    return(
-        Statistics::Lite::sum(@LIST),
-        Statistics::Lite::max(@LIST),
-        Statistics::Lite::min(@LIST),
-        Statistics::Lite::range(@LIST),
-        Statistics::Lite::mean(@LIST),
-    );
-};
-
-my $result = timethese( -1 => +{
-    'Swoop' => $swoop,
-    'Lite'  => $lite,
-});
-
-cmpthese $result;
